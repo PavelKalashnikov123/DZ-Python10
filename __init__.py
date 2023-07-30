9536112996664c3168b0ff91264f2cf41d9b2bea
PavelKalashnikov123 <pavel-kalashnikov@mail.ru>
2023-07-30 21:44:48 +0300

    init

---
+++ __init__.py
@@ -0,0 +1,11 @@
+from FunctionsToMethods import *
+from AnimalFabric import *
+from Animals import *
+from Human import *
+import Circle
+import CircleSet
+import Employee
+
+
+__all__ = ["FeelNumbers", "NameGen", "TwoFilesInOne", "FunctionsToMethods", "JsonToCsv", "GetFromUser", "Fish",
+           "Bird", "Dog", "Animals", "Human"]
